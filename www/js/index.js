85ab513879cd2523575a17a1f30efec566c33928
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2023-03-29 12:24:41 +0800

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,4 @@
-var language;
+var language = "eng";
 
 function getFacilityData() {
 
@@ -18,45 +18,34 @@ function showRoute(jsonData){
     var tbody = document.getElementById("records_table")
                         .getElementsByTagName("tbody")[0];
 
-//    // for test
-//    language = "";
+//    const localStorage = window.localStorage;
+//    if (localStorage) {
+//
+//        localStorage.setItem("Facility_Data", JSON.stringify());
+//
+//    }
 
-    switch (language) {
+//    document.getElementById("test_label").innerHTML = WalkingTracks;
 
-        case "zh_hk":
-            for (var i = 0; i < jsonData.length; i++){
+    if (language === "zh_hk"){
+        for (var i = 0; i < jsonData.length; i++){
+            var new_row = tbody.insertRow();
+            var new_cell = new_row.insertCell();
+            var new_text = document.createTextNode(jsonData[i].Title_tc);
+            new_cell.appendChild(new_text);
+            new_cell.setAttribute("onclick", "location.href='details.html'");
+            new_cell.setAttribute("value", i.toString());
+        }
+    } else {
+        for (var i = 0; i < jsonData.length; i++){
+            var new_row = tbody.insertRow();
+            var new_cell = new_row.insertCell();
+            var new_text = document.createTextNode(jsonData[i].Title_en);
+            new_cell.appendChild(new_text);
+            new_cell.setAttribute("onclick", "location.href='details.html'");
+            new_cell.setAttribute("value", i.toString());
+        }
 
-                var new_row = tbody.insertRow();
-                var new_cell = new_row.insertCell();
-                var new_text = document.createTextNode(jsonData[i].Title_tc);
-                new_cell.appendChild(new_text);
-                new_cell.setAttribute("onclick", "location.href='details.html'");
-                new_cell.setAttribute("value", i.toString());
-
-            }
-            break;
-        case "zh_cn":
-            for (var i = 0; i < jsonData.length; i++){
-
-                var new_row = tbody.insertRow();
-                var new_cell = new_row.insertCell();
-                var new_text = document.createTextNode(jsonData[i].Title_sc);
-                new_cell.appendChild(new_text);
-                new_cell.setAttribute("onclick", "location.href='details.html'");
-                new_cell.setAttribute("value", i.toString());
-            }
-            break;
-        default:
-            for (var i = 0; i < jsonData.length; i++){
-
-                var new_row = tbody.insertRow();
-                var new_cell = new_row.insertCell();
-                var new_text = document.createTextNode(jsonData[i].Title_en);
-                new_cell.appendChild(new_text);
-                new_cell.setAttribute("onclick", "location.href='details.html'");
-                new_cell.setAttribute("value", i.toString());
-            }
-            break;
     }
 
 };
