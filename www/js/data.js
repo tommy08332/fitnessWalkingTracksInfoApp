0033f7b916556643c36df69c7b60ec9f0d46db25
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2023-03-29 09:32:15 +0800

    Update data.js

--- www/js/data.js
+++ www/js/data.js
@@ -1,1 +1,23 @@
 // Getter and Setter
+//Getter and Setter
+class WalkingTracks{
+
+    constructor(tracksData){
+
+        this.tracksData = tracksData;
+
+    }
+
+    setTracksData (tracksData) {
+
+        this.tracksData = tracksData;
+
+    }
+
+    getTracksData () {
+
+        return this.tracksData;
+
+    }
+
+}
