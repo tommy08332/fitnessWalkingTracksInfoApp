fe56bab6bea99cc69f61a6b023c6dc92a9f546f6
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2023-03-29 21:24:42 +0800

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,55 +1,133 @@
-var language = "eng";
+function createLanguageType() {
 
-function getFacilityData() {
+    const localStorage = window.localStorage;
+    const language_type = localStorage.getItem("language_type");
 
-    const path = "facility-fw.json";
+    if (language_type){
+//        setText("zh_hk");
+        setText(language_type);
 
-    fetch (path)
-    .then(res => res.json())
-    .then(data => {
-        showRoute(data);
+    } else {
 
-    });
+        localStorage.setItem("language_type", "eng");
+        createLanguageType();
+
+    }
 
 };
 
-function showRoute(jsonData){
+//// for init or onclick
+function setText(language){
+
+    const localStorage = window.localStorage;
+    const language_type = localStorage.getItem("language_type");
+
+    if (language_type !== language){
+
+        localStorage.setItem("language_type", language);
+
+    }
+
+    if (language_type === "zh_hk"){
+
+        /// set text and button to traditional chinese
+//                document.getElementById("test_label").innerHTML = "language_type2";
+
+    } else {
+
+        /// set text and button to english
+
+//                document.getElementById("test_label").innerHTML = "language_type3";
+    }
+
+};
+
+// finish
+function getFacilityData() {
+
+    const localStorage = window.localStorage;
+    const facility_data = localStorage.getItem("facility_data");
+
+    if (facility_data){
+
+        var facilities = JSON.parse(facility_data)
+        showRoute(facilities);
+
+    }else{
+
+        const path = "facility-fw.json";
+
+        fetch (path)
+        .then(res => res.json())
+        .then(data => {
+
+            localStorage.setItem("facility_data", JSON.stringify(data));
+
+        });
+
+    }
+
+};
+
+// finish
+function showRoute(data){
 
     var tbody = document.getElementById("records_table")
                         .getElementsByTagName("tbody")[0];
 
-//    const localStorage = window.localStorage;
-//    if (localStorage) {
-//
-//        localStorage.setItem("Facility_Data", JSON.stringify());
-//
-//    }
-
 //    document.getElementById("test_label").innerHTML = WalkingTracks;
 
+    var language = localStorage.getItem("language_type");
+
     if (language === "zh_hk"){
-        for (var i = 0; i < jsonData.length; i++){
+        for (var i = 0; i < data.length; i++){
             var new_row = tbody.insertRow();
             var new_cell = new_row.insertCell();
-            var new_text = document.createTextNode(jsonData[i].Title_tc);
+            var new_text = document.createTextNode(data[i].Title_tc);
             new_cell.appendChild(new_text);
             new_cell.setAttribute("onclick", "location.href='details.html'");
-            new_cell.setAttribute("value", i.toString());
+//            new_cell.setAttribute("onclick", "changeToDetailPage()");
+            new_cell.setAttribute("value", data[i].Title_tc);
         }
     } else {
-        for (var i = 0; i < jsonData.length; i++){
+        for (var i = 0; i < data.length; i++){
             var new_row = tbody.insertRow();
             var new_cell = new_row.insertCell();
-            var new_text = document.createTextNode(jsonData[i].Title_en);
+            var new_text = document.createTextNode(data[i].Title_en);
             new_cell.appendChild(new_text);
             new_cell.setAttribute("onclick", "location.href='details.html'");
-            new_cell.setAttribute("value", i.toString());
+//            new_cell.setAttribute("onclick", "changeToDetailPage()");
+            new_cell.setAttribute("value", data[i].Title_en);
         }
 
     }
 
 };
 
+function changeToDetailPage(){
+
+
+
+//     document.getElementById("test_label").innerHTML = text;
+
+//     var tbody = document.getElementById("records_table");
+//     var tr = getElementsByTagName("tr");
+
+//     for (i = 0; i < tr.length; i++){
+
+//         var currentRow = tbody.row[i];
+
+//         document.getElementById("test_label").innerHTML = currentRow;
+//     }
+
+
+// //    document.getElementById("test_label").innerHTML = tr.textContent;
+
+}
+
 function search() {};
 
+createLanguageType();
+
 getFacilityData();
+
