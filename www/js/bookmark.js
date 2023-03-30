c7fc6b072b2e339fe29072fff1ce7de059048cf3
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2023-03-31 02:17:48 +0800

    Update bookmark.js

--- www/js/bookmark.js
+++ www/js/bookmark.js
@@ -1,8 +1,7 @@
-
-const homeButton = document.querySelector("#homebtn");
-const mapButton = document.querySelector("#mapbtn");
-const langButton = document.querySelector("#lang_bttn");
-const bookmarkButton = document.querySelector("#bookmarkbtn");
+const homeButton = document.querySelector("#homebtn"),
+      mapButton = document.querySelector("#mapbtn"),
+      langButton = document.querySelector("#lang_bttn"),
+      bookmarkButton = document.querySelector("#bookmarkbtn");
 
 bookmarkButton.onclick = function(){
     changePage("bookmark.html");
@@ -11,7 +10,6 @@ bookmarkButton.onclick = function(){
 homeButton.onclick = function(){
     changePage("index.html");
 }
-
 
 mapButton.onclick = function(){
     changePage("map.html");
@@ -29,7 +27,7 @@ langButton.onclick = function (){
 
     document.getElementById("lang_bttn").value = language;
     setText(language);
-    getFacilityData();
+    getBookmarkedRoute();
 
 }
 
@@ -45,135 +43,109 @@ function createLanguageType() {
     } else {
 
         localStorage.setItem("language_type", "eng");
-        createLanguageType();
+        getBookmarkedRoute();
 
     }
 
-
 };
-
 
 function setText(language){
 
     const localStorage = window.localStorage;
     let language_type = localStorage.getItem("language_type");
+    langButton.value = language;
 
-    if (language_type !== language){
+    if (language_type !== language) {
 
         localStorage.setItem("language_type", language);
         language_type = language;
 
     }
 
-    console.log("language_type    ", language_type);
-
-    if (language_type === "zh_hk"){
-
-        /// set picture
+    if (language_type === "zh_hk") {
 
         /// set text to traditional chinese
-        document.getElementById("headLbl").innerHTML = "健步行徑";
-        document.getElementById("mapbtn").innerHTML = "地圖";
-        document.getElementById("bookmarkbtn").innerHTML = "書籤";
-        document.getElementById("searchbtn").innerHTML = "搜尋";
-        document.getElementById("lang_bttn").innerHTML = "中文";
-        document.getElementById("search_input_field").placeholder = "搜尋位置";
-        document.getElementById("search_bttn").innerHTML = "搜尋";
+        document.getElementById("headLbl").innerHTML = "書籤";
+        document.getElementById("bookmark_th").innerHTML = "路徑"
+        document.getElementById("map_text").innerHTML = "地圖";
+        document.getElementById("bookmark_text").innerHTML = "書籤";
+        document.getElementById("home_text").innerHTML = "主頁";
+        document.getElementById("lang_text").innerHTML = "中文";
 
     } else {
-
-        /// set picture
 
         /// set text to english
-        document.getElementById("headLbl").innerHTML = "Fitness Walking Tracks";
-        document.getElementById("mapbtn").innerHTML = "Map";
-        document.getElementById("bookmarkbtn").innerHTML = "Bookmark";
-        document.getElementById("searchbtn").innerHTML = "Search";
-        document.getElementById("lang_bttn").innerHTML = "ENGLISH";
-        document.getElementById("search_input_field").placeholder = "Search location";
-        document.getElementById("search_bttn").innerHTML = "Search";
+        document.getElementById("headLbl").innerHTML = "Bookmark";
+        document.getElementById("bookmark_th").innerHTML = "Title"
+        document.getElementById("map_text").innerHTML = "Map";
+        document.getElementById("bookmark_text").innerHTML = "Bookmark";
+        document.getElementById("home_text").innerHTML = "Home";
+        document.getElementById("lang_text").innerHTML = "English";
     }
 
 };
 
-// finish
-function getFacilityData() {
+function getBookmarkedRoute(){
 
     const localStorage = window.localStorage;
-    const facility_data = localStorage.getItem("facility_data");
+    const bookmark_item = localStorage.getItem("bookmark_item");
 
-    if (facility_data){
+    if (bookmark_item) {
 
-        var facilities = JSON.parse(facility_data)
-        showRoute(facilities);
+        var table = document.getElementById("bookmark_items");
+        var tbody = table.getElementsByTagName("tbody");
 
-    }else{
-
-        const path = "facility-fw.json";
-
-        fetch (path)
-        .then(res => res.json())
-        .then(data => {
-
-            localStorage.setItem("facility_data", JSON.stringify(data));
-
-        });
-
-    }
-
-};
-
-// finish
-function showRoute(data){
-
-    var table = document.getElementById("records_table");
-    var tbody = table.getElementsByTagName("tbody");
-
-    if (tbody.length != 0){
-        tbody[0].parentNode.removeChild(tbody[0]);
-    }
-
-    table.appendChild(document.createElement("tbody"));
-    var language = localStorage.getItem("language_type");
-
-    if (language === "zh_hk"){
-        for (var i = 0; i < data.length; i++){
-            var new_row = tbody[0].insertRow();
-            var new_cell = new_row.insertCell();
-            var new_text = document.createTextNode(data[i].Title_tc);
-            new_cell.appendChild(new_text);
-            new_cell.setAttribute("onclick", "redirectDetailPage(this)");
-        }
-    } else {
-        for (var i = 0; i < data.length; i++){
-            var new_row = tbody[0].insertRow();
-            var new_cell = new_row.insertCell();
-            var new_text = document.createTextNode(data[i].Title_en);
-            new_cell.appendChild(new_text);
-            new_cell.setAttribute("onclick", "redirectDetailPage(this)");
+        if (tbody.length != 0){
+            tbody[0].parentNode.removeChild(tbody[0]);
         }
 
-    }
+        table.appendChild(document.createElement("tbody"));
+        var language = localStorage.getItem("language_type");
 
-};
+        var get_bookmark_item = JSON.parse(bookmark_item);
+
+        if (language === "zh_hk"){
+            for (var i = 0; i < get_bookmark_item.length; i++){
+
+                var new_row = tbody[0].insertRow();
+                var new_cell = new_row.insertCell();
+                var new_text = document.createTextNode(get_bookmark_item[i].Title_tc);
+                new_cell.appendChild(new_text);
+                new_cell.setAttribute("onclick", "redirectDetailPage(this)");
+            }
+        } else {
+
+            for (var i = 0; i < get_bookmark_item.length; i++){
+
+                var new_row = tbody[0].insertRow();
+                var new_cell = new_row.insertCell();
+                var new_text = document.createTextNode(get_bookmark_item[i].Title_en);
+                new_cell.appendChild(new_text);
+                new_cell.setAttribute("onclick", "redirectDetailPage(this)");
+
+            }
+
+        }
+    }
+}
 
 function redirectDetailPage(tableObj){
 
-//    document.getElementById("test_label").innerHTML = text.textContent;
     const localStorage = window.localStorage;
     localStorage.removeItem("selected_facility");
-    const facility_data = localStorage.getItem("facility_data");
+    const bookmark_items = localStorage.getItem("bookmark_item");
     const language_type = localStorage.getItem("language_type");
 
-    var facilities = JSON.parse(facility_data);
+    var bookmarks = JSON.parse(bookmark_items);
 
-    for (var i of facilities){
+    for (var i of bookmarks){
 
         if (language_type === "zh_hk" &&
             i.Title_tc === tableObj.textContent){
 
             localStorage.setItem("selected_facility", JSON.stringify(i));
             break;
+
         } else if (language_type === "eng" &&
                    i.Title_en === tableObj.textContent){
 
@@ -182,79 +154,10 @@ function redirectDetailPage(tableObj){
 
         }
     }
+
     changePage("details.html");
 
 }
-
-function search() {
-
-    var input = document.getElementById("search_input_field");
-    var filter = input.value.toUpperCase();
-    var tbody = document.getElementById("records_table");
-    var tr = tbody.getElementsByTagName("tr");
-
-//    for(var i =0; tr.length;i++){
-//         td = tr[i].getElementsByTagName("td")[0] ;
-//            if(td){
-//                var txtValue = td.textContent || td.innerText;
-//                if(txtValue.toUpperCase().indexOf(filter) >-1){
-//                        tr[i].style.display = "";
-//                    }else{
-//                       tr[i].style.display = "none";
-//                    }
-//            }
-//
-//    }
-    var searchbtn = document.getElementById("searchbtn");
-
-    const localStorage = window.localStorage;
-    let language_type = localStorage.getItem("language_type");
-
-    if (language_type === "zh_hk"){
-
-        searchbtn.innerHTML = "主頁";
-
-    } else{
-
-        searchbtn.innerHTML = "Home";
-
-    }
-    var searchbtn = document.getElementById("searchbtn");
-    searchbtn.setAttribute("onclick", "searchReset()");
-
-};
-
-//nav bar code here
-
-//search button
-function show_hide_searchbar(){
-
-    var hidesearch = document.getElementById("middle-part");
-
-    //hides search bar
-     if (hidesearch.style.display === "none") {
-        hidesearch.style.display = "block";
-      }else {
-           hidesearch.style.display = "none";
-      }
-
-}
-
-function searchReset(){
-
-    var hidesearch = document.getElementById("middle-part");
-    hidesearch.style.display = "none";
-
-    createLanguageType();
-    getFacilityData();
-
-    var searchbtn = document.getElementById("searchbtn");
-    search_bttn.setAttribute("onclick", "show_hide_searchbar()");
-
-    document.getElementById("search_input_field").innerHTML = "";
-
-}
-
 
 function changePage(page){
     window.location.href = page;
@@ -262,4 +165,4 @@ function changePage(page){
 
 createLanguageType();
 
-getFacilityData();
+getBookmarkedRoute();
